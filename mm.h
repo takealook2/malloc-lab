68011b1c6e4634a6840ed28df0327d06ba208bbb
root <root@seoha.>
2024-10-23 09:48:50 +0900

    mm.h 구현

--- mm.h
+++ mm.h
@@ -4,8 +4,6 @@ extern int mm_init (void);
 extern void *mm_malloc (size_t size);
 extern void mm_free (void *ptr);
 extern void *mm_realloc(void *ptr, size_t size);
-
-
 /* 
  * Students work in teams of one or two.  Teams enter their team name, 
  * personal names and login IDs in a struct of this
